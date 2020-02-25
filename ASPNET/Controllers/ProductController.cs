1c8015b4ea55bf2aed6c1197cfc2f8dc06acba5c
Alexis Diaz <chexisss@gmail.com>
2020-02-25 09:05:44 -0600

    addedUpdateProduct

--- ASPNET/Controllers/ProductController.cs
+++ ASPNET/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNET.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNET.Controllers
@@ -28,5 +29,24 @@ namespace ASPNET.Controllers
             return View(product);
         }
 
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+
+            repo.UpdateProduct(prod);
+
+            if(prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
     }
 }
--- ASPNET/ProductRepository.cs
+++ ASPNET/ProductRepository.cs
@@ -26,6 +26,13 @@ namespace ASPNET
                 new { id = id });
 
         }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE Products SET Name = @name, Price = @price WHERE ProductID = @id",
+
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
     }
 
 }
