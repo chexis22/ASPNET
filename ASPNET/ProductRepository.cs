cf7b761d120151d0758486260400f4dcb7b773c2
Alexis Diaz <chexisss@gmail.com>
2020-02-25 10:08:58 -0600

    Added InsertProduct

--- ASPNET/ProductRepository.cs
+++ ASPNET/ProductRepository.cs
@@ -33,6 +33,24 @@ namespace ASPNET
 
                 new { name = product.Name, price = product.Price, id = product.ProductID });
         }
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+
     }
 
 }
